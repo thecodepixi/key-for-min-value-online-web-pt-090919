c4c23ac7b16b724b4192147209043ff0d95e7141
Emily Harber <emily.a.harber@gmail.com>
2019-09-17 23:42:01 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,11 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value(name_hash)
+def key_for_min_value(name_hash = nil)
   smallest = nil 
   smallest_key = ""
  name_hash.each do |key, value|
-   if name_hash.empty? 
+   if name_hash == nil  
      return nil 
    elsif smallest == nil || value < smallest
      smallest = value 
