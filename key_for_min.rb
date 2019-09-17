83007e1bbb268a1382a90fb0af8a20ca9f20d9ad
Emily Harber <emily.a.harber@gmail.com>
2019-09-17 23:43:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,12 +4,16 @@
 def key_for_min_value(name_hash)
   smallest = nil 
   smallest_key = ""
- name_hash.each do |key, value|
-   if smallest == nil || value < smallest
-     smallest = value 
-     smallest_key = key 
-   end 
- end 
+  if name_hash.empty? 
+    return nil 
+  else 
+    name_hash.each do |key, value|
+      if smallest == nil || value < smallest
+        smallest = value 
+        smallest_key = key 
+      end 
+    end 
+  end 
  
  smallest_key
  
