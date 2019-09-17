4715b9d87b4fc38dd49834f487abe50393b2b592
Emily Harber <emily.a.harber@gmail.com>
2019-09-17 23:40:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,17 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  smallest = nil 
+  smallest_key = ""
+ name_hash.each do |key, value|
+   if name_hash.empty? 
+     return nil 
+   elsif smallest == nil || value < smallest
+     smallest = value 
+     smallest_key = key 
+   end 
+ end 
+ 
+ smallest_key
+ 
 end
